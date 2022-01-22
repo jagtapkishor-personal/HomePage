1a275d81bd3b7866bcdf54329dbbb46e970bcb7d
Kishor Dattatraya Jagtap <jagtapkishor104@gmail.com>
2022-01-22 10:28:34 +0530

    HomePage With Cover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HomePage';
+
+  imagesrc=[{
+    "imagepath":"sm-image-1.jpg",
+    "alt":"Image Not found",
+    "text":"image1"
+  },
+  {
+    "imagepath":"sm-image-2.jpg",
+    "alt":"Image Not found"
+  },
+  {
+    "imagepath":"sm-image-3.jpg",
+    "alt":"Image Not found"
+  },
+  {
+    "imagepath":"sm-image-4.jpg",
+    "alt":"Image Not found"
+  },
+  {
+    "imagepath":"sm-image-5.jpg",
+    "alt":"Image Not found"
+  },
+  {
+    "imagepath":"sm-image-6.jpg",
+    "alt":"Image Not found"
+  },
+  {
+    "imagepath":"sm-image-7.jpg",
+    "alt":"Image Not found"
+  },
+  {
+    "imagepath":"sm-image-8.jpg",
+    "alt":"Image Not found"
+  },
+  {
+    "imagepath":"sm-image-9.jpg",
+    "alt":"Image Not found"
+
+  },
+  {
+    "imagepath":"sm-image-10.jpg",
+    "alt":"Image Not found"
+  },
+]
 }
